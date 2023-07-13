9dbeaafa7be725ba0ed39e68865abd5d623ec54f
Pramod6395 <pramod.patil@cimcondigital.com>
2023-07-13 11:24:33 +0530

    Model Evaluation Stage added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainin
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationPipeline
 from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 from textSummarizer.logging import logger
 
 
@@ -42,9 +43,22 @@ except Exception as e:
 
 STAGE_NAME = "Model Trainer stage"
 try:
+    logger.info(f"*******************")
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     moderl_trainer = ModelTrainerTrainingPipeline()
     moderl_trainer.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Evaluation stage"
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    moderl_evaluation = ModelEvaluationTrainingPipeline()
+    moderl_evaluation.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=========x")
 except Exception as e:
     logger.exception(e)
